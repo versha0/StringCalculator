62b0aba204fef21c25617beb63c9fcc685ae970d
versha0 <versha.verma@gmail.com>
2021-01-06 16:24:41 +0530

    corrected logic for two or more numbers

--- src/main/java/org/versha/calculator/StringCalculator.java
+++ src/main/java/org/versha/calculator/StringCalculator.java
@@ -4,7 +4,11 @@ public class StringCalculator
 {
     public int add(String string){
         if(string.equals("")) return 0;
-
-        return Integer.parseInt(string);
+        String[] arrayString = string.split(",");
+        int ans =0;
+        for(int i=0;i<arrayString.length;i++){
+            ans = ans+Integer.parseInt(arrayString[i]);
+        }
+        return ans;
     }
 }
