1633b082445dd74510e4717ba1712487a8edaf39
versha0 <versha.verma@gmail.com>
2021-01-06 14:40:18 +0530

    made new project

---
+++ src/main/java/org/versha/calculator/StringCalculator.java
@@ -0,0 +1,13 @@
+package org.versha.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringCalculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/versha/calculator/StringCalculatorTest.java
@@ -0,0 +1,20 @@
+package org.versha.calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class StringCalculatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
