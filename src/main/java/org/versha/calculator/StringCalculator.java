2b75e7a0951cdd23f35f0e9686f1490b22af3a02
versha0 <versha.verma@gmail.com>
2021-01-06 16:46:48 +0530

    corrected logic for \n in the string in add method

--- src/main/java/org/versha/calculator/StringCalculator.java
+++ src/main/java/org/versha/calculator/StringCalculator.java
@@ -4,6 +4,8 @@ public class StringCalculator
 {
     public int add(String string){
         if(string.equals("")) return 0;
+        string = string.replaceAll("\n",",");
+        System.out.println(string);
         String[] arrayString = string.split(",");
         int ans =0;
         for(int i=0;i<arrayString.length;i++){
